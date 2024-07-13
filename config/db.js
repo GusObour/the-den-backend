6512906ef67842feb75a99b84801414a9bdc52a8
GusObour <obourgod@gmail.com>
2024-07-13 09:36:14 -0500

    trying to fix mongo connection error

--- config/db.js
+++ config/db.js
@@ -9,9 +9,7 @@ class Database {
     try {
       this.mongooseConnection = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
+        useUnifiedTopology: true
     });
       console.log('Database connected successfully');
       return this.mongooseConnection;
